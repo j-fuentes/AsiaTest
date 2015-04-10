4326ce4c214fb8f57a50393269e3573e01458b12
jfc <jotaefe555555@gmail.com>
2015-04-10 12:01:43 +0200

    Running services

--- master/puppet/modules/apache/manifests/init.pp
+++ master/puppet/modules/apache/manifests/init.pp
@@ -1,5 +1,6 @@
 class apache {
 
+
   $phpmodules = [ "php", "php-mysql", "php-gd", "php-imap", "php-ldap", "php-odbc", "php-pear", "php-xml", "php-xmlrpc", "php-mbstring", "php-mcrypt", "php-mssql", "php-snmp", "php-soap", "php-tidy", "curl", "php-pecl-apc"]
   $javapackages = [ "java-1.6.0-openjdk", "java-1.6.0-openjdk-devel" ]
   $dependencies = [ "cyrus-sasl-devel", "zlib-devel", "gcc-c++" ]
@@ -13,21 +14,29 @@ class apache {
   exec { "update-yum": path => ['/usr/bin', '/usr/sbin', '/bin'], command => "yum update -y", }
 
   # install curl-devel. This bypass yum list checking that returns !0 and makes puppet stop the installation of this package.
-  exec { "install-curl-devel": path => ['/usr/bin', '/usr/sbin', '/bin'], command => "yum install -y curl-devel", require => Exec["update-yum"], }
+  exec { "install-curl-devel": path => ['/usr/bin', '/usr/sbin', '/bin'], command => "yum install -y curl-devel", require => Exec["update-yum"], unless => "yum list installed libcurl-devel",}
 
   # install php-pecl-memcached.x86_64. This bypass yum list checking that returns !0 and makes puppet stop the installation of this package.
-  exec { "install-php-pecl-memcached.x86_64": path => ['/usr/bin', '/usr/sbin', '/bin'], command => "yum install -y php-pecl-memcached.x86_64", require => Exec["update-yum"], }
+  exec { "install-php-pecl-memcached.x86_64": path => ['/usr/bin', '/usr/sbin', '/bin'], command => "yum install -y php-pecl-memcached.x86_64", require => [ Exec["epelrepo"], Exec["update-yum"] ], unless => "yum list installed php-pecl-memcached.x86_64", }
 
   # add EPEL and REMI repo (needed to install some modules)
-  exec { "epelrepo": path => ['/usr/bin', '/usr/sbin', '/bin'], cwd => "/tmp", command => "wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm && wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm && rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm && yum update -y", unless => "ls /tmp/remi-release-6*.rpm",}
+  exec { "epelrepo":
+        path => ['/usr/bin', '/usr/sbin', '/bin'], cwd => "/tmp",
+        command => "wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm && wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm && rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm && yum update -y",
+        unless => "ls /tmp/remi-release-6*.rpm",
+      }
 
+  # install apache
+  package { 'httpd': ensure => "installed", require => Exec["update-yum"],}
+  # install php modules
+  package { $phpmodules: ensure => "installed", require => [ Exec["epelrepo"], Exec["update-yum"] ], }
 
-  package { $phpmodules: ensure => "installed", require => [ Exec["update-yum"], Exec["epelrepo"] ], }
+  # install java
   package { $javapackages: ensure => "installed", require => Exec["update-yum"], }
+
+  # install libmemcached stuff
   package { $dependencies: ensure => "installed", require => Exec["update-yum"], }
   package { $libmemcaches: ensure => "installed", require => Exec["update-yum"], }
-
-
 
    # Unpack libmemcached
    exec {"unpackLibMemcached":
@@ -45,7 +54,7 @@ class apache {
      command     => "/tmp/libmemcached-1.0.16/configure --disable-memcached-sasl && make && make install",
      logoutput   => true,
      refreshonly => true,
-     notify      => Exec["installMemcached"]
+     notify      => Exec["installMemcached"],
      }
 
    # Install memcached with pecl
@@ -66,32 +75,35 @@ class apache {
        refreshonly => true
        }
 
-  # Apache and common modules from pupept core
-  include apache
-
+  # Apache configuration
   file {'/etc/httpd/conf/httpd.conf':
     ensure => file,
     source => "puppet:///modules/apache/etc/httpd/conf/httpd.conf",
+    require => Package['httpd'],
   }
 
   file {'/etc/httpd/conf/magic':
     ensure => file,
     source => 'puppet:///modules/apache/etc/httpd/conf/magic',
+    require => Package['httpd'],
   }
 
   file {'/etc/httpd/conf.d/munin.conf':
     ensure => file,
     source => 'puppet:///modules/apache/etc/httpd/conf.d/munin.conf',
+    require => Package['httpd'],
   }
 
   file {'/etc/httpd/conf.d/php.conf':
     ensure => file,
     source => 'puppet:///modules/apache/etc/httpd/conf.d/php.conf',
+    require => Package['httpd'],
   }
 
   file {'/etc/httpd/conf.d/welcome.conf':
     ensure => file,
     source => 'puppet:///modules/apache/etc/httpd/conf.d/welcome.conf',
+    require => Package['httpd'],
   }
 
   file {'/etc/munin/':
@@ -102,6 +114,22 @@ class apache {
     ensure => file,
     source => 'puppet:///modules/apache/etc/munin/munin.conf',
     require => File["/etc/munin"],
+  }
+
+  # Services
+  service { 'httpd':
+    ensure => running,
+    enable => true,
+  }
+
+  service { 'sshd':
+    ensure => running,
+    enable => true,
+  }
+
+  service { 'vsftpd':
+    ensure => running,
+    enable => true,
   }
 
 }
